0517b66fc04def9d91faebd7dcd0d4913c6315a4
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-01-21 10:44:54 +0100

    Repo done

--- S2.WpfBasic/EmployeeRepository.cs
+++ S2.WpfBasic/EmployeeRepository.cs
@@ -1,11 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace S2.WpfBasic
 {
     public class EmployeeRepository
     {
-        private 
+        private List<Employee> employees;
+
+        public EmployeeRepository()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee {Firstname = "Brian", Lastname = "Jørgensen", HireDate = new DateTime(2017, 08, 01), YearlySalary = 10000m},
+                new Employee {Firstname = "Ole", Lastname = "Bay Jensen", HireDate = new DateTime(2005, 01, 01), YearlySalary = 12000m},
+            };
+        }
+
+        public void Add(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void Delete(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return employees;
+        }
+
+        public Employee GetSingleBy(string fullname)
+        {
+            return employees.SingleOrDefault(e => String.Concat(e.Firstname, " ", e.Lastname) == fullname);
+        }
     }
 }
