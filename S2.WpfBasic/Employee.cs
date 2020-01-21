ab6c1afff4b996a4d8d759a17baddb232c33fb36
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-01-21 10:38:36 +0100

    Added employee and repo.

---
+++ S2.WpfBasic/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S2.WpfBasic
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public decimal YearlySalary { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
